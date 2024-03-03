ba6564334f0a2387ac425c93893d4ce438cba725
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-03-03 11:49:54 +0000

    object1

---
+++ 11_obj1.js
@@ -0,0 +1,38 @@
+//singleton
+//object.create => Constructor Method
+
+// object literals
+
+const mySym=Symbol("Key1") 
+
+const JsUser={
+    name:"Hitesh",
+    "Full Name":"Sushil Subedi",
+    age:18,
+    [mySym]:"mykey1",
+    location:"Pokhara",
+    email:"pokhara@google.com",
+    isLoggedIn:false,
+    lastLoginDaysa:["Monday","Saturday"]
+}
+// console.log(JsUser.name);
+// console.log(JsUser["name"]);
+// console.log(JsUser["Full Name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email="pokhara@chatgpt.com"
+// console.log(JsUser);
+
+// Object.freeze(JsUser)
+JsUser.email="pokhara@facebook.com"
+// console.log(JsUser);
+
+JsUser.greeting=function(){
+console.log("Hello Js User");
+}
+console.log(JsUser.greeting());
+
+JsUser.greetingTwo=function(){
+    console.log(`Hello Js User, ${this.name}`);
+    }
+console.log(JsUser.greetingTwo());
