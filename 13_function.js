5de256970d0e1301815f3694630ce0ea7aeeb473
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-03-12 15:05:20 +0000

    rest op, object in function and array in function handling

--- 13_function.js
+++ 13_function.js
@@ -33,13 +33,44 @@ function logInUserMessage(userName="sam"){
         console.log("please enter a username");
         return //return is used for termination of operation
     }
-    return `${userName} just loggined In`
+    return `${userName} just logged In`
 }
 
 // console.log(logInUserMessage("Sushil"));
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
 
 
 // POint To REMember
 // after return no operation take place.
+
+
+function calculateCartPrice(...num){
+    return num
+}
+// console.log(calculateCartPrice(100,200,300,400))
+//... is a rest operator and also called spread operator too.
+
+const user={
+    name:"hitesh",
+    price:199
+}
+function handleObj(anyobj){
+console.log(`name is ${anyobj.name} and price is ${anyobj.price}`);
+}
+// handleObj(user)
+
+//you can also pass object directly too.
+handleObj({
+    name:"Sarala",
+    price:399
+})
+
+const myNewArray=[100,200,300,400]
+
+function returnSecondvalue(secondvalue){
+return secondvalue[1]
+}
+console.log(returnSecondvalue(myNewArray));
+//Also you can pass array directly also.
+console.log(returnSecondvalue([100,200,300,400]));
 
