4c8be726924bfccfbb3421bc4be2716a0c068a93
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-03-15 10:46:34 +0000

    read before .js

---
+++ 17_codeexecutionandcallsrack.js
@@ -0,0 +1,25 @@
+// Javascript Execution Context
+
+// 1.Global Execution Context (refered by this variable)
+
+// Global execution context of browser is window object.
+
+// 2.Function Execution Context
+
+// 3.Eval Execution Context
+
+// ---------How code executed------------
+// 1.Global Execution Phase
+// 2.memory creation phase
+// 3.Execution phase
+
+// In Memory creation phase variables are assigned undefined and function's definition is loaded.
+
+// In Execution phase, variables get values as per input and function definition has no role.Context
+
+// In Execution phase, if function is called it creates a new execution context(new variable environment and execution thread)
+// These  memory creation phase and execution phase for function.
+// After execution the result is returned to global execution phase with delete of that new execution context.
+
+
+// If not understand look video😂
