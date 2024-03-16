6b30557b8ebc5d5b294bfea8eb1c6604694eb286
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-03-16 13:47:02 +0000

    while loop

---
+++ 03_iteration/While_and_do_while.js
@@ -0,0 +1,23 @@
+// ---------while--------
+
+let i=0
+while (i<=10) {
+    // console.log(i);
+    i=i+2
+}
+const myArray=["flash","spiderman","Batsman"]
+i=0
+while(i<myArray.length){
+    // console.log(myArray[i]);
+    i++
+}
+
+// ----------------------------do-while--------------------
+
+let h=1
+do {
+    console.log(h);
+    h++
+} while (h<=10);
+
+// Another topic is array loop
