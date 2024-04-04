82785db1b363238b1d081f8217265eeb21e08ef0
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-04-04 07:16:37 +0000

    oop

---
+++ 10_classes_and_OOP/oop.js
@@ -0,0 +1,35 @@
+const user = {
+    username: "Sushil",
+    loginCount: 8,
+    signedIn: true,
+    getUserDetail: function () {
+        console.log(`Username:${this.username}`);
+        console.log(this);
+    }
+
+}
+
+// console.log(user.username);
+// user.getUserDetail()
+
+// console.log(this); //=> {}
+
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting= function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 7, true)  //see removing new of both once unexpected will happen
+const userTwo = new User("sarala", 8, false)
+// console.log(userOne);
+console.log(userOne.constructor);
+// console.log(userTwo); 
+
